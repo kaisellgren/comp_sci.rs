c5a6096520c8da274aac524fc5ffe8f2b8c12f69
Kai Sellgren <kaisellgren@gmail.com>
2014-11-16 16:31:16 +0200

    Implement selection sort

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -1,3 +1,5 @@
+use std::num::Bounded;
+
 pub fn insertion_sort<'a, T: Ord + 'a>(data: &'a mut [T]) {
     let size = data.len();
     if size < 2 { return; }
@@ -9,4 +11,22 @@ pub fn insertion_sort<'a, T: Ord + 'a>(data: &'a mut [T]) {
             x2 -= 1;
         }
     }
+
+pub fn selection_sort<'a, T: Ord + Bounded + 'a>(data: &'a mut [T]) {
+    let size = data.len();
+    if size < 2 { return; }
+
+    let mut current_min: Option<uint> = None;
+    let ref max: T = Bounded::max_value();
+
+    for x in range(0, size) {
+        for x2 in range(x, size) {
+            if &data[x2] < current_min.map_or(max, |current| &data[current]) {
+                current_min = Some(x);
+                data.swap(x, x2);
+            }
+        }
+        current_min = None;
+    }
+}
 }
